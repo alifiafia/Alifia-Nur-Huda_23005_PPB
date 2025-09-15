2674d367886860dac369eac9a683eecc6e2fdec2
alifiafia <alifianurhuda2@gmail.com>
2025-09-15 10:40:32 +0700

    first commit

---
+++ lib/daftar_mahasiswa.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DaftarMahasiswaPage extends StatelessWidget {
+  const DaftarMahasiswaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Daftar Mahasiswa")),
+      body: const Center(
+        child: Text("Halaman daftar mahasiswa masih kosong."),
+      ),
+    );
+  }
+}
